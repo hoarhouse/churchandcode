5531da7cba6e081cd30a1c0b7ef083910a3357bd
GitHub Action <action@github.com>
2026-05-17 13:52:28 -0600

    Add cluster auto-discovery system: dynamic homepage, sitemap.xml, llms.txt

--- .eleventy.js
+++ .eleventy.js
@@ -2,13 +2,37 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/CNAME");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
-  eleventyConfig.addPassthroughCopy("src/sitemap.xml");
-  eleventyConfig.addPassthroughCopy("src/llms.txt");
 
-  eleventyConfig.addGlobalData("permalink", () => {
-    return (data) => {
+  eleventyConfig.addGlobalData("eleventyComputed", {
+    permalink: (data) => {
+      if (data.permalink) {
+        return data.permalink;
+      }
       return data.page.filePathStem.replace(/^\//, '') + (data.page.outputFileExtension ? '.' + data.page.outputFileExtension : '');
-    };
+    }
+  });
+
+  const clusters = [
+    "pope-leo-xiv",
+    "ai-and-being-human",
+    "ai-and-kids",
+    "ai-and-work",
+    "ai-and-the-world",
+    "reference"
+  ];
+
+  clusters.forEach(clusterName => {
+    eleventyConfig.addCollection(clusterName, function(collectionApi) {
+      return collectionApi.getAll()
+        .filter(item => item.data.cluster === clusterName)
+        .sort((a, b) => (b.data.date || 0) - (a.data.date || 0));
+    });
+  });
+
+  eleventyConfig.addCollection("allCornerstone", function(collectionApi) {
+    return collectionApi.getAll()
+      .filter(item => item.data.cluster)
+      .sort((a, b) => (b.data.date || 0) - (a.data.date || 0));
   });
 
   return {
